86c81ac952b26625b2bc836ba388575f896bff31
SuperKai <307140566@qq.com>
2021-10-14 22:17:22 +0800

    Spring微服务之间的调用Demo

--- cloud-provider-payment8001/src/main/java/com/singed/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/singed/springcloud/controller/PaymentController.java
@@ -22,24 +22,24 @@ public class PaymentController {
     private PaymentService paymentService;
 
     @PostMapping(value = "/payment/create")
-    public CommonResult create(Payment payment) {
+    public CommonResult<Integer> create(Payment payment) {
         int result = paymentService.create(payment);
         log.info("******插入结果：" + result);
         if (result > 0) {
-            return new CommonResult(200, "插入数据库成功", result);
+            return new CommonResult<>(200, "插入数据库成功", result);
         } else {
-            return new CommonResult(444, "插入数据库失败", null);
+            return new CommonResult<>(444, "插入数据库失败", null);
         }
     }
 
     @GetMapping(value = "payment/get/{id}")
-    public CommonResult getPaymentById(@PathVariable("id") Long id) {
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
         Payment result = paymentService.getPaymentById(id);
         log.info("******插入结果：" + result);
         if (result != null) {
-            return new CommonResult(200, "查询成功", result);
+            return new CommonResult<>(200, "查询成功", result);
         } else {
-            return new CommonResult(444, "没有对应记录，查询ID: " + id, null);
+            return new CommonResult<>(444, "没有对应记录，查询ID: " + id, null);
         }
     }
 }
