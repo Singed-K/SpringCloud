1fc569a6ca816601f47190ee2ff1fe099a8dc672
SuperKai <307140566@qq.com>
2021-10-15 00:44:29 +0800

    Eureka集群配置 order调用payment实现负载均衡

--- cloud-provider-payment8002/src/main/java/com/singed/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/com/singed/springcloud/controller/PaymentController.java
@@ -4,6 +4,7 @@ import com.singed.springcloud.entities.CommonResult;
 import com.singed.springcloud.entities.Payment;
 import com.singed.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -18,12 +19,15 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @PostMapping(value = "/payment/create")
     public CommonResult<Integer> create(@RequestBody Payment payment) {
         int result = paymentService.create(payment);
         log.info("******插入结果：" + result);
         if (result > 0) {
-            return new CommonResult<>(200, "插入数据库成功", result);
+            return new CommonResult<>(200, "插入数据库成功,serverPort: " + serverPort, result);
         } else {
             return new CommonResult<>(444, "插入数据库失败", null);
         }
@@ -34,7 +38,7 @@ public class PaymentController {
         Payment result = paymentService.getPaymentById(id);
         log.info("******插入结果：" + result);
         if (result != null) {
-            return new CommonResult<>(200, "查询成功", result);
+            return new CommonResult<>(200, "查询成功,serverPort: " + serverPort, result);
         } else {
             return new CommonResult<>(444, "没有对应记录，查询ID: " + id, null);
         }
