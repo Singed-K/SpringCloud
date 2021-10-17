66f426103731363bdba945623956bd9d330138eb
SuperKai <307140566@qq.com>
2021-10-18 00:22:23 +0800

    Hystrix Payment微服务构建

---
+++ cloud-provider-hystrix-payment8001/src/main/java/com/singed/springcloud/PaymentHystrixMain8001.java
@@ -0,0 +1,17 @@
+package com.singed.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author : Singed
+ * @create : 2021/10/18 0:18
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class PaymentHystrixMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentHystrixMain8001.class, args);
+    }
+}
