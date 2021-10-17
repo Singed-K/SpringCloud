38eb44d71c83f1fc824f8866b6e6bdba151b935a
SuperKai <307140566@qq.com>
2021-10-18 00:45:42 +0800

    Hystrix consumer微服务构建

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/singed/springcloud/OrderHystrixMain80.java
@@ -0,0 +1,17 @@
+package com.singed.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author : Singed
+ * @create : 2021/10/18 0:41
+ */
+@SpringBootApplication
+@EnableFeignClients
+public class OrderHystrixMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderHystrixMain80.class, args);
+    }
+}
