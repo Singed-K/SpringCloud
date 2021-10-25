ff4ce930c5af891de75b1942c2bf077593173e04
SuperKai <307140566@qq.com>
2021-10-25 23:16:18 +0800

    gateway filter Demo

---
+++ cloud-config-center-3344/src/main/java/com/singed/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,17 @@
+package com.singed.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author : Singed
+ * @create : 2021/10/25 21:46
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
