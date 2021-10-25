ac736757fbb82bea181714bed6e8840fc54d175b
SuperKai <307140566@qq.com>
2021-10-26 00:51:19 +0800

    springcloud config Demo

---
+++ cloud-config-client-3355/src/main/java/com/singed/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,17 @@
+package com.singed.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author : Singed
+ * @create : 2021/10/25 23:56
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
