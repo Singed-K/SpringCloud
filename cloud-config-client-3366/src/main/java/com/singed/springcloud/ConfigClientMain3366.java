7e90be9b95627402e0466d186bed1350eb0caced
SuperKai <307140566@qq.com>
2021-10-27 00:28:15 +0800

    config定点更新config

---
+++ cloud-config-client-3366/src/main/java/com/singed/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,17 @@
+package com.singed.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author : Singed
+ * @create : 2021/10/26 22:29
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
--- cloud-config-client-3366/src/main/java/com/singed/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3366/src/main/java/com/singed/springcloud/controller/ConfigClientController.java
@@ -7,15 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author : Singed
- * @create : 2021/10/25 23:57
+ * @create : 2021/10/26 22:31
  */
 @RestController
 @RefreshScope
 public class ConfigClientController {
-    /**
-     * 调用以下url刷新config
-     * curl -X POST "http://localhost:3355/actuator/refresh"
-     */
     @Value("${server.port}")
     private String serverPort;
     @Value("${config.info}")
