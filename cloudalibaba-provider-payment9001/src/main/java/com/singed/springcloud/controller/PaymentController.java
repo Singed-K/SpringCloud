7128f8ba45ba38cf1ff08c588b7c1518a333ad43
SuperKai <307140566@qq.com>
2021-10-28 19:38:11 +0800

    cloudalibaba payment注册进nacos

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/singed/springcloud/controller/PaymentController.java
@@ -0,0 +1,23 @@
+package com.singed.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : Singed
+ * @create : 2021/10/28 19:24
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/nacos/{id}")
+    public String getPayment(@PathVariable("id") Integer id) {
+        return "nacos registry, serverPort: " + serverPort + "\t id" + id;
+    }
+}
