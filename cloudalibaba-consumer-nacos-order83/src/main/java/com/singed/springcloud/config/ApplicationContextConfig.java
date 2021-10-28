8e6989bcedc33d5571bf4a7c1a176faf6afb0a9f
SuperKai <307140566@qq.com>
2021-10-28 23:00:10 +0800

    cloudalibaba payment注册进nacos

---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/singed/springcloud/config/ApplicationContextConfig.java
@@ -0,0 +1,19 @@
+package com.singed.springcloud.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author : Singed
+ * @create : 2021/10/28 19:42
+ */
+@Configuration
+public class ApplicationContextConfig {
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+}
